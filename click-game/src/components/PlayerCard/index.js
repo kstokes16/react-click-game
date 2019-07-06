bbb66c65d2370ea97302015a61dfded1d0b43bbb
Kevin Stokes <kevinstokes@Kevins-MBP.attlocal.net>
2019-07-06 00:12:28 -0400

    cards display 6 per row

--- click-game/src/components/PlayerCard/index.js
+++ click-game/src/components/PlayerCard/index.js
@@ -5,11 +5,11 @@ import "./style.css";
 function PlayerCard (props) {
     return (
         
-        <div className="img-fluid img-thumbnail col-lg-3" id="player-card">
-          <img src={props.image} id="player-pic" alt={props.name} onClick={() => props.tally(props.id)}/>
+        <div className="col-lg-2 col-md-3 col-sm-6" id="player-card">
+          <img className="img-fluid img-thumbnail" src={props.image} id="player-pic" alt={props.name} onClick={() => props.tally(props.id)}/>
           <div className="card-body">
-            <h5 className="card-title" id="player-name">Name: {props.name}</h5>
-            <h5 className="card-title" id="player-position">Position: {props.position}</h5>
+            <h5 className="card-title text-center" id="player-name">{props.name}</h5>
+            <h5 className="card-title text-center" id="player-position">{props.position}</h5>
           </div>
         </div>
     )
