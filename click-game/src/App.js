9fb2bf1193a84d19e1054ee8d0e0f9583b891174
Kevin Stokes <kevinstokes@MacBook-Pro.attlocal.net>
2019-07-07 14:21:36 -0400

    functionality works

--- click-game/src/App.js
+++ click-game/src/App.js
@@ -10,34 +10,93 @@ import Jumbotron from "./components/Jumbotron";
 class App extends Component {
   // perform all JS logic before the render function
   state = {
-    players: players,
-    guess: null,
+    players,
     score: 0,
     topScore: 0,
-    currentClicks: []
+    clickedPlayer: []
   };
 
-  handleIncrement = () => {
-    this.setState({ score: this.state.score + 1 });
-    if (this.state.topScore < this.state.score) {
-      this.setState({ topScore: this.state.score + 1 })
+  handleIncrement = event => {
+    const currentPlayer = event.target.alt;
+    const playerAlreadyClicked = this.state.clickedPlayer.indexOf(currentPlayer) > -1;
+
+    if (playerAlreadyClicked) {
+      this.setState(
+        {
+        players: this.state.players.sort(function(a, b) {
+          return 0.5 - Math.random();
+        }
+        ),
+        clickedPlayer: [],
+        score: 0
+      });
+
+      alert("You lose. Play again?");
+
+      if (this.state.score > this.state.topScore)
+      {
+      this.setState({ topScore: this.state.score})
+      }
+    } 
+    else {
+      this.setState(
+        {
+          players: this.state.players.sort(function(a, b) {
+            return 0.5 - Math.random();
+          }),
+          clickedPlayer: this.state.clickedPlayer.concat(currentPlayer),
+          score: this.state.score + 1
+        },
+
+        () => {
+          if (this.state.score === 12)
+          {
+            alert("Yay! You Win!");
+            this.setState(
+              {
+              players: this.state.players.sort(function(a, b)
+              {
+                return 0.5 - Math.random();
+              }),
+              currentPlayer: [],
+              score: 0
+            });
+          }
+        });
     }
-    this.shuffle(players);
   };
 
-  shuffle = (cards) => {
-    let currentIndex = cards.length, temporaryValue, randomIndex;
-    while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      temporaryValue = cards[currentIndex];
-      cards[currentIndex] = cards[randomIndex];
-      cards[randomIndex] = temporaryValue;
-    }
-    return cards;
-  }
+  // handleIncrement = id => {
+  //   this.setState({ score: this.state.score + 1 });
+  //   if (this.state.topScore < this.state.score) {
+  //     this.setState({ topScore: this.state.score + 1 })
+  //   }
+  //   this.shuffle(players);
+  // };
 
-  // update score on click of card
+  // end of on click function
+
+  // start of card shuffle
+  // shuffle = (cards) => {
+  //   let currentIndex = cards.length, temporaryValue, randomIndex;
+  //   while (0 !== currentIndex) {
+  //     randomIndex = Math.floor(Math.random() * currentIndex);
+  //     currentIndex -= 1;
+  //     temporaryValue = cards[currentIndex];
+  //     cards[currentIndex] = cards[randomIndex];
+  //     cards[randomIndex] = temporaryValue;
+  //   }
+  //   return cards;
+  // }
+
+  // run this if a card has been clicked more than once
+  // gameOver = () => {
+  //   this.setState({ score: 0 });
+  //   if (this.state.score > this.state.topScore) {
+  //     this.setState({ topScore: this.state.score})
+  //   }
+  //   this.shuffle(players);
+  // }
 
   // map through the PlayersCard component and render cards for each
   render() {
@@ -47,6 +106,7 @@ class App extends Component {
       score={this.state.score}
       topScore={this.state.topScore}/>
       <div className="container">
+      <br/>
       <div className="row">
       {this.state.players.map(player => (
       <PlayerCard 
--- click-game/src/components/PlayerCard/index.js
+++ click-game/src/components/PlayerCard/index.js
@@ -4,13 +4,13 @@ import "./style.css";
 
 function PlayerCard (props) {
     return (
-        <div className="col-lg-2 col-md-3 col-sm-6" id="player-card">
-          <img className="img-fluid img-thumbnail" src={props.image} id="player-pic" alt={props.name} onClick={() => props.handleIncrement(props.id)}/>
+      <div className="col-lg-2 col-md-3 col-sm-6" id="player-card">
+          <img className="img-fluid img-thumbnail" src={props.image} id="player-pic" alt={props.id} onClick={props.handleIncrement}/>
           <div className="card-body">
             <h5 className="card-title text-center" id="player-name">{props.name}</h5>
             <h5 className="card-title text-center" id="player-position">{props.position}</h5>
           </div>
-        </div>
+      </div>
     )
 }
 
