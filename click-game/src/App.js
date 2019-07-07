5994725fd4bc650a87989fae97fe8a537400e987
Kevin Stokes <kevinstokes@Kevins-MBP.attlocal.net>
2019-07-06 20:03:36 -0400

    shuffle feature working

--- click-game/src/App.js
+++ click-game/src/App.js
@@ -5,12 +5,36 @@ import Container from "./components/Container";
 import Header from "./components/Header";
 import players from "./players.json";
 import PlayerCard from "./components/PlayerCard";
+import Jumbotron from "./components/Jumbotron";
 
 class App extends Component {
   // perform all JS logic before the render function
   state = {
-    players
+    players: players,
+    guess: null,
+    score: 0,
+    topScore: 0,
+    currentClicks: []
   };
+
+  handleIncrement = () => {
+    this.setState({ score: this.state.score + 1 });
+    this.shuffle(players);
+  };
+
+  shuffle = (cards) => {
+    let currentIndex = cards.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = cards[currentIndex];
+      cards[currentIndex] = cards[randomIndex];
+      cards[randomIndex] = temporaryValue;
+    }
+    return cards;
+  }
+
+  // update score on click of card
 
   // map through the PlayersCard component and render cards for each
   render() {
@@ -21,7 +45,8 @@ class App extends Component {
       <div className="row">
       {this.state.players.map(player => (
       <PlayerCard 
-      tally={this.tally}  
+      score={this.state.score}
+      handleIncrement={this.handleIncrement}  
       id={player.id}
       key={player.id}
       image={player.image}
--- click-game/src/components/PlayerCard/index.js
+++ click-game/src/components/PlayerCard/index.js
@@ -4,9 +4,8 @@ import "./style.css";
 
 function PlayerCard (props) {
     return (
-        
         <div className="col-lg-2 col-md-3 col-sm-6" id="player-card">
-          <img className="img-fluid img-thumbnail" src={props.image} id="player-pic" alt={props.name} onClick={() => props.tally(props.id)}/>
+          <img className="img-fluid img-thumbnail" src={props.image} id="player-pic" alt={props.name} onClick={() => props.handleIncrement(props.id)}/>
           <div className="card-body">
             <h5 className="card-title text-center" id="player-name">{props.name}</h5>
             <h5 className="card-title text-center" id="player-position">{props.position}</h5>
