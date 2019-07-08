74ffe21fce61dd77a164f1ee8886f33e00a58d73
Kevin Stokes <kevinstokes@Kevins-MacBook-Pro.local>
2019-07-08 19:10:36 -0400

    moving repo to deploy to gh pages

---
+++ build/precache-manifest.520dbdde7502be25f7ed9b19c8133a99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41a48c5ed14aa2371855e7754cea7f3",
+    "url": "/kstokes16/react-click-game/index.html"
+  },
+  {
+    "revision": "bd64aacc5539dda78e3e",
+    "url": "/kstokes16/react-click-game/static/css/main.ce696a64.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/kstokes16/react-click-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "bd64aacc5539dda78e3e",
+    "url": "/kstokes16/react-click-game/static/js/main.449fa72c.chunk.js"
+  },
+  {
+    "revision": "44790bc5487ce6211a8c",
+    "url": "/kstokes16/react-click-game/static/js/runtime~main.c0324dff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kstokes16/react-click-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
